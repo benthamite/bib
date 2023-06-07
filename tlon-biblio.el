a7fe5fcc988fe8cd27c61f96e2f2e62a5b8528d7
Pablo Stafforini <pablo@stafforini.com>
2023-06-07 16:22:01 -0300

    Various changes

--- tlon-biblio.el
+++ tlon-biblio.el
@@ -53,25 +53,25 @@
 							 " & "))
 					  (title (car (alist-get 'title item)))
 					  (doi (alist-get 'DOI item)))
-                                      (cons (concat title " by " (tlon-biblio-reverse-first-last-name author-names)) doi)))
+				      (cons (concat title " by " (tlon-biblio-reverse-first-last-name author-names)) doi)))
 				  items))
-              (selected-string (completing-read "Select a bibliographic entry: " candidates))
-              (selected-doi (cdr (assoc selected-string candidates))))
+	      (selected-string (completing-read "Select a bibliographic entry: " candidates))
+	      (selected-doi (cdr (assoc selected-string candidates))))
     selected-doi))
 
 (defun tlon-biblio-search-crossref (title &optional author)
   "Query the Crossref database for TITLE and AUTHOR."
   (let* ((url-request-method "GET")
-         (url (concat (format "https://api.crossref.org/works?query.bibliographic=%s"
+	 (url (concat (format "https://api.crossref.org/works?query.bibliographic=%s"
 			      (url-hexify-string title))
 		      (when author
 			(format "&query.author=%s"
 				(url-hexify-string author)))))
-         (url-buffer (url-retrieve-synchronously url))
-         (json-string (with-current-buffer url-buffer
-                        (goto-char (point-min))
-                        (re-search-forward "^$")
-                        (buffer-substring-no-properties (point) (point-max)))))
+	 (url-buffer (url-retrieve-synchronously url))
+	 (json-string (with-current-buffer url-buffer
+			(goto-char (point-min))
+			(re-search-forward "^$")
+			(buffer-substring-no-properties (point) (point-max)))))
     (message (tlon-biblio-get-doi-in-json json-string))))
 
 (defvar tlon-biblio-isbndb-key
@@ -80,29 +80,34 @@
 (defun tlon-biblio-search-isbndb (title)
   "Query the ISBNdb database for TITLE."
   (let* ((url (format "https://api2.isbndb.com/books/%s?page=1&pageSize=20" (url-hexify-string title)))
-         (url-request-method "GET")
-         (url-request-extra-headers
+	 (url-request-method "GET")
+	 (url-request-extra-headers
 	  `(("Accept" . "application/json")
 	    ("Authorization" . ,tlon-biblio-isbndb-key)))
-         (url-buffer (url-retrieve-synchronously url))
-         (json-object-type 'plist)
-         (json-key-type 'keyword)
-         (json-array-type 'list)
-         json-data
-         result-list)
+	 (url-buffer (url-retrieve-synchronously url))
+	 (json-object-type 'plist)
+	 (json-key-type 'keyword)
+	 (json-array-type 'list)
+	 json-data
+	 result-list)
     (with-current-buffer url-buffer
       (goto-char (point-min))
       (search-forward "\n\n") ;; Skip response headers
       (setq json-data (json-read)))
     (setq result-list (plist-get json-data :books))
     (unless result-list
-      (error "No results found"))
+      (user-error "No results found"))
     (let* ((candidates (mapcar (lambda (book)
-                                 (cons (format "%s by %s" 
-                                               (plist-get book :title) 
-					       (tlon-biblio-reverse-first-last-name (car (plist-get book :authors)))
-					       ", ")
-				       (plist-get book :isbn)))
+				 (let* ((title (plist-get book :title))
+					(authors (plist-get book :authors))
+					(isbn (plist-get book :isbn)))
+				   (cons (if authors
+					     (format "%s by %s"
+						     title
+						     (tlon-biblio-reverse-first-last-name (car authors))
+						     ", ")
+					   (format "%s, " title))
+					 isbn)))
 			       result-list))
 	   (selection (completing-read "Select a book: " candidates)))
       (cdr (assoc selection candidates)))))
@@ -112,12 +117,10 @@
   (interactive)
   (let ((type (completing-read "DOI or ISBN? " '("doi" "isbn") nil t))
 	(title (read-string "Enter title: "))
-	author)
-    (when (string= type "doi")
-      (setq author (read-string "Enter author: ")))
+	(author (read-string "Enter author: ")))
     (if-let (identifier (if (string= type "doi")
 			    (tlon-biblio-search-crossref title (unless (string= author "") author))
-			  (tlon-biblio-search-isbndb title)))
+			  (tlon-biblio-search-isbndb (format "%s %s" title author))))
 	(zotra-add-entry-from-search identifier)
       (user-error "No entries found"))))
 
