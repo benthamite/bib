910a5fd7304a9bc3217085508061f3653e8ccc66
Pablo Stafforini <pablo@stafforini.com>
2023-06-07 15:41:56 -0300

    Add tlon-biblio-isbndb-key

--- tlon-biblio.el
+++ tlon-biblio.el
@@ -68,15 +68,56 @@
                         (buffer-substring-no-properties (point) (point-max)))))
     (tlon-biblio-get-doi-in-json json-string)))
 
+(defvar tlon-biblio-isbndb-key
+  (auth-source-pass-get "key" (concat "tlon/BAE/isbndb.com/" ps/tlon-email)))
+
+(defun tlon-biblio-search-isbndb (title)
+  "Docstring."
+  (let* ((url (format "https://api2.isbndb.com/books/%s?page=1&pageSize=20" (url-hexify-string title)))
+         (url-request-method "GET")
+         (url-request-extra-headers
+	  `(("Accept" . "application/json")
+	    ("Authorization" . ,tlon-biblio-isbndb-key)))
+         (url-buffer (url-retrieve-synchronously url))
+         (json-object-type 'plist)
+         (json-key-type 'keyword)
+         (json-array-type 'list)
+         json-data
+         result-list)
+
+    (with-current-buffer url-buffer
+      (goto-char (point-min))
+      (search-forward "\n\n") ;; Skip response headers
+      (setq json-data (json-read)))
+
+    (setq result-list (plist-get json-data :books))
+
+    (unless result-list
+      (error "No results found"))
+
+    (let* ((candidates (mapcar (lambda (book)
+                                 (cons (format "%s by %s" 
+                                               (plist-get book :title) 
+					       (car (plist-get book :authors))
+					       ", ")
+				       (plist-get book :isbn)))
+			       result-list))
+	   (selection (completing-read "Select a book: " candidates)))
+
+      (cdr (assoc selection candidates)))))
+
 (defun tlon-biblio-zotra-add-entry-from-title ()
   "Prompt user for title and author and add selection to bibfile via its associated DOI."
   (interactive)
-  (let ((title (read-string "Enter title: "))
-        (author (read-string "Enter author: ")))
-    (if-let ((doi (tlon-biblio--query-crossref title
-					       (unless (string= "" author)
-						 author))))
-	(zotra-add-entry-from-search doi)
+  (let ((type (completing-read ": " '("doi" "isbn") nil t))
+	(title (read-string "Enter title: "))
+	author)
+    (when (string= type "doi")
+      (setq author (read-string "Enter author: ")))
+    (if-let ((identifier (if (string= type "doi")
+			     (tlon-biblio-search-crossref title author)
+			   (tlon-biblio-search-isbndb title))))
+	(zotra-add-entry-from-search identifier)
       (message "No DOI entries found for the given title and author."))))
 
 (provide 'tlon-biblio)
